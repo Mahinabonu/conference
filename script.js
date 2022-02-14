c0db9cda5cca18aee2391145a05503dc0c4885b6
MAHINA <mahinabekova@gmail.com>
2022-02-14 18:19:29 +0500

    "register" responsive

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    menu.style.display="block";
+})
+
